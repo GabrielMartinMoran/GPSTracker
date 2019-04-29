94d190e08340ea5fb01a811fa2293755be72591c
Germán Torós <cybercat_666_78@hotmail.com>
2019-04-29 19:31:31 -0300

    Implementado GPS.actualizar()

--- gpstracker-cpp/include/GPS.h
+++ gpstracker-cpp/include/GPS.h
@@ -1,10 +1,13 @@
 #include "Mockups/ControladorGPSMockup.h"
+#include "Utils/StringTokenizer.h"
 
 class GPS {
     private:
         ControladorGPSMockup *controladorGPSMockup;
         float latitud = 0;
         float longitud = 0;
+        boolean N = true;
+        boolean W = true;
     public:
         GPS(ControladorGPSMockup *controladorGPSMockup);
         void actualizar();
--- gpstracker-cpp/src/GPS.cpp
+++ gpstracker-cpp/src/GPS.cpp
@@ -1,10 +1,27 @@
 #include "GPS.h"
 
 void GPS::actualizar(){
+    int latitudAux;
+    int longitudAux;
+    boolean NAux;
+    boolean WAux;
     String datos = controladorGPSMockup->leerInformacion();
-    latitud = 3751.65;
-    longitud = 14507.36;
-};
+    StringTokenizer tokens(datos, ",");
+    if(tokens.nextToken() == "$PGRMC"){
+        tokens.nextToken(); // Time of fix 22:54:46 UTC
+        if(tokens.nextToken() == "A"){
+            latitud = tokens.nextToken().toFloat();
+            N = tokens.nextToken() == "N";
+            longitud = tokens.nextToken().toFloat();
+            W = tokens.nextToken() == "W";
+            //tokens.nextToken(); Speed over ground, Knots
+            //tokens.nextToken(); Course Made Good, True
+            //tokens.nextToken(); Date of fix  19 November 1994
+            //tokens.nextToken(); Magnetic variation 20.3 deg East
+            //tokens.nextToken(); mandatory checksum
+        }
+    }
+}
 
 GPS::~GPS(){
     delete controladorGPSMockup;
