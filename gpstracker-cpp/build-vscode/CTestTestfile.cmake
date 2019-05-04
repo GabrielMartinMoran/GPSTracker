bdb1767326de46b42b6500b17e79995011cec8f8
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2019-05-04 19:56:12 -0300

    Borrada carpeta build

--- gpstracker-cpp/build-vscode/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: B:/GPSTracker/gpstracker/gpstracker-cpp
-# Build directory: B:/GPSTracker/gpstracker/gpstracker-cpp/build-vscode
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
--- gpstracker-cpp/build-vscode/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: B:/GPSTracker/gpstracker/gpstracker-cpp/googletest/googletest
-# Build directory: B:/GPSTracker/gpstracker/gpstracker-cpp/build-vscode/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
