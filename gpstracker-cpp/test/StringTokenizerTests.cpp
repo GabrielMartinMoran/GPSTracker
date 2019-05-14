376f75370c658400910d4c41fec04f4ea92a1f84
Germán Torós <cybercat_666_78@hotmail.com>
2019-05-14 16:57:36 -0300

    fix test StringTokenizer

--- gpstracker-cpp/test/StringTokenizerTests.cpp
+++ gpstracker-cpp/test/StringTokenizerTests.cpp
@@ -7,51 +7,51 @@ TEST(StringTokenizer, tokens)
     StringTokenizer tokens(command, ",");
     std::string esperado = "$GPRMC";
     std::string obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "081836";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "A";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "3751.65";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "S";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "14507.36";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "E";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "000.0";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "360.0";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "130998";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "011.3";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     esperado = "E*63";
     obtenido = tokens.nextToken();
-    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+    EXPECT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
                                   << "obtenido: " << obtenido;
     
 }
