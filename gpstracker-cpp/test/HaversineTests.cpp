630fc910c9ded6c136d558d2de56a677f1109f6e
Germán Torós <cybercat_666_78@hotmail.com>
2019-05-15 23:17:36 -0300

    HaversineTests

---
+++ gpstracker-cpp/test/HaversineTests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <utils/Haversine.h>
+
+TEST(Haversine, haversine_m){
+    double lat1 = 51.5007;
+    double lon1 = 0.1246;
+    double lat2 = 40.6892;
+    double lon2 = 74.0445;
+    double esperado;
+    double obtenido;
+
+    esperado = 5571340.3215750661;
+    obtenido = haversine_m(lat1, lon1, lat2, lon2);
+    ASSERT_EQ(esperado, obtenido) << "esperado: " << esperado << "\n"
+                                  << "obtenido: " << obtenido;
+}
