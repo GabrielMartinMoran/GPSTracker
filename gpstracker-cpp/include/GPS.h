f4fdcad7a48da1755e5d7bda2ad815567327e05c
Germán Torós <cybercat_666_78@hotmail.com>
2019-05-16 01:30:20 -0300

    Creado GPSData

--- gpstracker-cpp/include/GPS.h
+++ gpstracker-cpp/include/GPS.h
@@ -3,24 +3,15 @@
 
 #include <iostream>
 #include <interfaces/IGPSController.h>
-#include <utils/StringTokenizer.h>
 #include <utils/Haversine.h>
-#include <utils/StringToNumber.h>
+#include <GPSData.h>
 
 class GPS
 {
 private:
     IGPSController *GPSController;
-    int hora = 0;
-    int minuto = 0;
-    int segundo = 0;
-    int dia = 0;
-    int mes = 0;
-    int anio = 0;
-    double latitud = 0;
-    double longitud = 0;
-    void parsearTiempo(std::string tiempo, int *horaDia, int *minutoMes, int *sengundoAnio);
-    bool posicionValida(double latitud, double longitud);
+    GPSData gpsData = GPSData(std::string(""));
+    bool posicionValida(GPSData gpsData);
 public:
     GPS(IGPSController *GPSController);
     void actualizar();
--- gpstracker-cpp/src/GPS.cpp
+++ gpstracker-cpp/src/GPS.cpp
@@ -5,85 +5,59 @@ GPS::GPS(IGPSController *GPSController)
     this->GPSController = GPSController;
 }
 
-bool GPS::posicionValida(double latitud, double longitud)
+bool GPS::posicionValida(GPSData gpsData)
 {
-    return haversine_m(latitud, longitud, this->latitud, this->longitud) > 0;
+    return haversine_m(this->gpsData.getLatitud(), this->gpsData.getLongitud(), gpsData.getLatitud(), gpsData.getLongitud()) > 2.5;
 }
 
 void GPS::actualizar()
 {
     std::string datos = this->GPSController->getInformation();
-    StringTokenizer tokens(datos, ",");
-    if (tokens.nextToken() == "$GPRMC")
-    {
-        std::string tiempo = tokens.nextToken(); // 08:18:36 UTC
-        parsearTiempo(tiempo, &(this->hora), &(this->minuto), &(this->segundo));
-        if (tokens.nextToken() == std::string("A"))
-        {
-            this->latitud = stringToNumber<double>(tokens.nextToken());
-            if (tokens.nextToken() == std::string("S"))
-            {
-                this->latitud *= -1;
-            }
-            this->longitud = stringToNumber<double>(tokens.nextToken());
-            if (tokens.nextToken() == std::string("E"))
-            {
-                this->longitud *= -1;
-            }
-            tokens.nextToken();                     //Speed over ground, Knots
-            tokens.nextToken();                     //Course Made Good, True
-            std::string fecha = tokens.nextToken(); //13 Sep 1998
-            parsearTiempo(fecha, &(this->dia), &(this->mes), &(this->anio));
-            tokens.nextToken(); //Magnetic variation 20.3 deg East
-            tokens.nextToken(); //mandatory checksum
+    GPSData nuevo = GPSData(datos);
+    if(nuevo.isValido()){
+        if(posicionValida(nuevo)){
+            this->gpsData = nuevo;
         }
     }
 }
 
-void GPS::parsearTiempo(std::string tiempo, int *horaDia, int *minutoMes, int *sengundoAnio)
-{
-    *horaDia = stringToNumber<int>(tiempo.substr(0, 2));
-    *minutoMes = stringToNumber<int>(tiempo.substr(2, 2));
-    *sengundoAnio = stringToNumber<int>(tiempo.substr(4, 2));
-}
-
 int GPS::getHora()
 {
-    return this->hora;
+    return this->gpsData.getHora();
 }
 
 int GPS::getMinuto()
 {
-    return this->minuto;
+    return this->gpsData.getMinuto();
 }
 
 int GPS::getSegundo()
 {
-    return this->segundo;
+    return this->gpsData.getSegundo();
 }
 
 int GPS::getDia()
 {
-    return this->dia;
+    return this->gpsData.getDia();
 }
 
 int GPS::getMes()
 {
-    return this->mes;
+    return this->gpsData.getMes();
 }
 
 int GPS::getAnio()
 {
-    return this->anio;
+    return this->gpsData.getAnio();
 }
 
 double GPS::getLatitud()
 {
-    return this->latitud;
+    return this->gpsData.getLatitud();
 }
 double GPS::getLongitud()
 {
-    return this->longitud;
+    return this->gpsData.getLongitud();
 }
 
 GPS::~GPS()
