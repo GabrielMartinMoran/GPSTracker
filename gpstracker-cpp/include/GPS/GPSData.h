f3a01e6672b087d49da2e8a4cc36fa7a34c7252e
Germán Torós <cybercat_666_78@hotmail.com>
2019-05-27 03:30:45 -0300

    GPSData usando heap

--- gpstracker-cpp/include/GPS/GPSData.h
+++ gpstracker-cpp/include/GPS/GPSData.h
@@ -14,7 +14,7 @@ class GPSData
 private:
     DateTime date_time = DateTime(1,1,0,0,0,0);
     Coordenada coordenada = Coordenada(0,0);
-    void parsearTiempo(std::string tiempo, unsigned int *horaDia, unsigned int *minutoMes, unsigned int *sengundoAnio);
+    void parsearTiempo(std::string *tiempo, unsigned int *horaDia, unsigned int *minutoMes, unsigned int *sengundoAnio);
     double parsearCoordenada(std::string coordenada);
     bool valido = false;
     std::string rawData;
